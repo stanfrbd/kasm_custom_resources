34e24fa9b21305a189cf414ce7d842a01e912aae
Stanislas MEDRANO <stanislas.medrano@hotmail.com>
2022-03-23 10:08:32 +0100

    Try to fix Chromium

--- src/install/chromium/install_chromium.sh
+++ src/install/chromium/install_chromium.sh
@@ -7,8 +7,15 @@ apt-get update
 apt-get install -y software-properties-common
 apt-get install -y chromium
 
-sed -i 's/-stable//g' /usr/share/applications/chromium.desktop
-sed -i 's#Exec=/usr/bin/chromium %U#Exec=/usr/bin/chromium %U --no-sandbox##' /usr/share/applications/chromium.desktop
+mv /usr/bin/chromium /usr/bin/chromium-orig
+cat >/usr/bin/chromium <<EOL
+#!/usr/bin/env bash
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"None"/' ~/.config/chromium/Default/Preferences
+/usr/bin/chromium-browser-orig ${CHROME_ARGS} "\$@"
+EOL
+chmod +x /usr/bin/chromium
+cp /usr/bin/chromium /usr/bin/chromium-browser
 
 cp /usr/share/applications/chromium.desktop $HOME/Desktop/
 chown 1000:1000 $HOME/Desktop/chromium.desktop
