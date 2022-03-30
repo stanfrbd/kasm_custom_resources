05f6b6417e8ad4cbb0fdfe1adc412bc012804565
s3ntine1 <s3ntine1@orion-server.eu>
2022-03-30 13:42:19 +0000

    Added tools

--- src/install/tools/install_tools_deluxe.sh
+++ src/install/tools/install_tools_deluxe.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 apt-get update
-apt-get install -y git tmux openvpn chromium nano vim zip unzip 7zip inetutils-ping build-essential libreoffice
+apt-get install -y git tmux openvpn chromium nano vim zip unzip 7zip inetutils-ping build-essential libreoffice qbittorrent
+
 
