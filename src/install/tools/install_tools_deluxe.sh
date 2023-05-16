cf19a0ea849e8bec42ad6644a6eb0a8254d15b28
stanfrbd <stan@local.test>
2023-05-16 09:13:59 +0000

    Changed Kali core-kali-rolling to tag 1.13.0-rolling for better updates

--- src/install/tools/install_tools_deluxe.sh
+++ src/install/tools/install_tools_deluxe.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 apt-get update
-apt-get install -y git tmux openvpn chromium nano vim zip unzip 7zip inetutils-ping build-essential libreoffice qbittorrent
+apt-get install -y git tmux openvpn chromium nano vim zip unzip \ 
+7zip inetutils-ping build-essential libreoffice qbittorrent
 
 
